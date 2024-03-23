66392445ed31a52ac1aa4bef809acf80f98f7af0
Thomas Skjerdal <skjerdalthomas@gmail.com>
2024-03-23 21:28:18 +0100

    Svelte ting

---
+++ src/components/Knapp.svelte
@@ -0,0 +1,21 @@
+<script>
+    function toggle() {
+        window.document.body.classList.toggle('lightmode')
+    }
+</script>
+
+<button on:click={toggle}>
+    <slot />
+</button>
+
+<style>
+    	button {
+		background-color: #f76027;
+		color: white;
+		border: none;
+		border-radius: 4px;
+		padding: 0.5rem;
+		text-transform: uppercase;
+		
+	}
+</style>
